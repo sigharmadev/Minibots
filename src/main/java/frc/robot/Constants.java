babfb62e8d5a7956e1f8c2f0e61ef9a265474a74
Dev Sharma <singh.sharma.dev@gmail.com>
2026-06-10 18:54:10 -0700

    added mode and robot types

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,53 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Constants {
+    
+    public static enum Mode {
+        /** Running on a real robot. */
+        REAL,
+        
+        /** Running a physics simulator. */
+        SIM,
+        
+        /** Replaying from a log file. */
+        REPLAY
+    }
+
+    public static enum RobotType {
+        COMPETITION, // The competition robot (with aluminum base)
+
+        /** The Sim Bot */
+        SIMBOT
+    }
+    
+    // This is the current robot type, change for running on real robot
+    private static final RobotType robotType = RobotType.SIMBOT;    
+    
+    // This is only a fallback! This will not change the robot type.
+    private static final RobotType defaultRobotType = RobotType.COMPETITION;
+
+    private static final Alert invalidRobotType = new Alert(
+        "Invalid RobotType selected. Defaulting to " + defaultRobotType.toString(),
+        AlertType.kWarning
+    );
+
+    public static RobotType getRobot() {
+        if (RobotBase.isReal() && robotType == RobotType.SIMBOT) {
+            invalidRobotType.set(true);
+            return defaultRobotType;
+        }
+
+        return robotType;
+    }
+
+    public static final Mode getMode() {
+        return switch(getRobot()) {
+            case SIMBOT -> Mode.SIM;
+            default -> RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
+        };
+    }    
+}
