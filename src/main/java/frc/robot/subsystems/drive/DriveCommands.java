6e7b35e93dd5d8c78f5d5c54c74fa850d0e77f74
Dev Sharma <singh.sharma.dev@gmail.com>
2026-08-27 09:31:39 -0700

    Added slow mode dpad driving commands

--- src/main/java/frc/robot/subsystems/drive/DriveCommands.java
+++ src/main/java/frc/robot/subsystems/drive/DriveCommands.java
@@ -198,7 +198,7 @@ public class DriveCommands {
           boolean isFlipped = DriverStation.getAlliance().isPresent()
               && DriverStation.getAlliance().get() == Alliance.Red;
           
-          drive.bypassDuty(
+          drive.runVelocity(
             speeds
           );
         },
